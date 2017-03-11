9825025f3e28d88a2045d9bb7b2b0d0681517903
Jake Cameron Booth <jakecameronbooth@Jakes-MacBook-Pro.local>
2017-03-11 17:48:51 +0000

    Can add items and pops back to main screen, Cannot complete or delete yet

---
+++ DoIt App/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt App
+//
+//  Created by Jake Cameron Booth on 11/03/2017.
+//  Copyright © 2017 Jake Cameron Booth. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
